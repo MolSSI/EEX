06712a8085204a219fd2ec3d3641cd5c3ada534c
Eliseo Marin <meliseo@vt.edu>
2017-09-19 16:35:06 -0400

    Modify bond fields
    Former-commit-id: 2117becb4d979865c951cf092956e4692a770308

--- eex/translators/lammps_coeff.py
+++ eex/translators/lammps_coeff.py
@@ -32,58 +32,69 @@ _bond_styles = {
     },
     "nonlinear": {
         "form": "(epsilon*(r-r0)^2) / (lambda^2-(r-r0)^2)",
-        "variables": ["r"],
-        "terms": ["epsilon", "r0", "lambda"],
-        "epsilon": "energy",
-        "r0": "distance",
-        "lambda": "distance"
+        "terms": OrderedDict({
+            "epsilon": "energy",
+            "r0": "distance",
+            "lambda": "distance"
+        }),
+        "description": "This is a nonlinear bond"
     },
     "zero": {},
     "fene/expand": {
         "form": "-0.5*K*R0^2*ln(1-((r-delta)/R0)^2 + 4*epsilon((sigma/(r-delta))^12 - (sigma/(r-delta))^6)) + epsilon",
-        "variables": ["r"],
-        "terms": ["K", "r0", "epsilon", "sigma", "delta"],
-        "K": "energy distance^-2",
-        "r0": "distance",
-        "epsilon": "energy",
-        "sigma": "distance",
-        "delta": "distance,"
+        "terms": OrderedDict({ 
+            "K": "energy distance^-2",
+            "r0": "distance",
+            "epsilon": "energy",
+            "sigma": "distance",
+            "delta": "distance"
+        }),
+        "description": "This is fene/expand bond"
     },
     "quartic": {
         "form": "K(r-Rc)^2 * (r-Rc-B1)*(r-Rc-B2) + U0 + 4*epsilon*((sigma/r)^12 - (sigma/r)^6) + epsilon",
         "variables": ["r"],
-        "terms": ["K", "B1", "B2", "Rc", "U0"],
-        "K": "energy distance^-4",
-        "B1": "distance",
-        "B2": "distance",
-        "Rc": "distance",
-        "U0": "energy"
+        "terms": OrderedDict({ 
+            "K": "energy distance^-4",
+            "B1": "distance",
+            "B2": "distance",
+            "Rc": "distance",
+            "U0": "energy"
+        }),
+        "description": "This is a quartic bond"
     },
     "hybrid": {},  # Special case - allows for more than one bond type in a simulation
     "harmonic": {
         "form": "K*(r-r0)^2",
         "variables": ["r"],
-        "terms": ["K", "r0"],
-        "K": "energy distance^-2",
-        "r0": "distance"
+        "terms": OrderedDict({ 
+            "K": "energy distance^-2",
+            "r0": "distance"
+        }),
+        "description": "This is a harmonic bond"
     },
     "table": {},  # Special case - creation of interpolation tables.
     "class2": {
         "form": "K2 * (r-r0)^2 + K3 * (r-r0)^3 + K4 * (r-r0)^4",
         "variables": ["r"],
-        "terms": ["R0", "K2", "K3", "K4"],
-        "R0": "distance",
-        "K2": "energy distance^-2",
-        "K3": "energy distance^-3",
-        "K4": "energy distnace^-4",
+        "terms": OrderedDict({ 
+            "R0": "distance",
+            "K2": "energy distance^-2",
+            "K3": "energy distance^-3",
+            "K4": "energy distnace^-4"
+        }),
+        "description": "This is a class2 bond"
     },
     "morse": {
         "form": "D * (1 - e^(-alpha * (r-r0)))^2",
         "variables": ["r"],
-        "terms": ["D", "alpha", "r0"],
-        "D": "energy",
-        "alpha": "distance^-1",
-        "r0": "distance"
+        "terms": OrderedDict({ 
+            "D": "energy",
+            "alpha": "distance^-1",
+            "r0": "distance"
+        }),
+        "description": "This is a class2 bond"
+        
     },
 }
 
