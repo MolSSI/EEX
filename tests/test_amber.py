Charge scaling for amber and fix amber_tests
Former-commit-id: 756d104d4e052a10544eb166182e371aec4d59b9

tests/test_amber.py
@@ -30,7 +30,7 @@ def test_amber_spce_read_atoms_value(spce_dl):
         ["atom_name", "charge", "atomic_number", "mass", "residue_name", "residue_index"], by_value=True)
     assert atoms.shape[0] == 648
     assert set(np.unique(atoms["atom_name"])) == set(["H1", "H2", "O"])
-    assert np.allclose(np.unique(atoms["charge"]), [-1.54452215E+01, 7.72261074E+00])
+    assert np.allclose(np.unique(atoms["charge"]), [-0.8476, 0.4238])
     assert np.allclose(np.unique(atoms["atomic_number"]), [1.0, 8.0])
     assert np.allclose(np.unique(atoms["mass"]), [1.008, 16.0])
     assert set(np.unique(atoms["residue_name"])) == set(["WAT"])
@@ -39,7 +39,7 @@ def test_amber_spce_read_atoms_value(spce_dl):
 
 
 # Test AMBER read by_index
-def test_amber_spce_read_atoms_value(spce_dl):
+def test_amber_spce_read_atoms_index(spce_dl):
     data, dl = spce_dl
 
     atoms = dl.get_atoms(