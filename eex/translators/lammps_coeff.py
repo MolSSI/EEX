fad5364349e453cad896545ae2cd67134d45c0d6
Daniel Smith <malorian@me.com>
2017-09-20 13:08:22 -0400

    Adds additional internal units for LAMMPS
    Former-commit-id: 7ea5ca916de4cf7e4e666d5c6521de0d5028a526

--- eex/translators/lammps_coeff.py
+++ eex/translators/lammps_coeff.py
@@ -1,9 +1,11 @@
-# Dictionaries for coeff format and units - http://lammps.sandia.gov/doc/Section_commands.html
+"""
+Dictionaries for coeff format and units - http://lammps.sandia.gov/doc/Section_commands.html
+"""
 
 from collections import OrderedDict
 
 # NYI
-_valid_bond_variables = {"r": {"units": "distance", "description": "Distance between the two index atoms"}}
+_valid_bond_variables = {"r": {"units": "[distance]", "description": "Distance between the two index atoms"}}
 
 # Valid columns of dataframe
 _valid_bond_indices = {
@@ -21,80 +23,80 @@ _valid_bond_indices = {
 _bond_styles = {
     "none": {},
     "fene": {
-        "form": "-0.5*K*R0^2*ln(1-(r/R0)^2) + 4*epsilon((sigma/r)^12 - (sigma/r)^6)) + epsilon",
+        "form": "-0.5*K*R0 ** 2*ln(1-(r/R0) ** 2) + 4*epsilon((sigma/r) ** 12 - (sigma/r) ** 6)) + epsilon",
         "terms": OrderedDict({
-            "K": "energy distance ^ -2",
-            "R0": "distance",
-            "epsilon": "energy",
-            "sigma": "distance"
+            "K": "[energy] [distance] ** -2",
+            "R0": "[distance]",
+            "epsilon": "[energy]",
+            "sigma": "[distance]"
         }),
         "description": "This is a fene bond!"
     },
     "nonlinear": {
-        "form": "(epsilon*(r-r0)^2) / (lambda^2-(r-r0)^2)",
+        "form": "(epsilon*(r-r0) ** 2) / (lambda ** 2-(r-r0) ** 2)",
         "terms": OrderedDict({
-            "epsilon": "energy",
-            "r0": "distance",
-            "lambda": "distance"
+            "epsilon": "[energy]",
+            "r0": "[distance]",
+            "lambda": "[distance]"
         }),
         "description": "This is a nonlinear bond"
     },
     "zero": {},
     "fene/expand": {
-        "form": "-0.5*K*R0^2*ln(1-((r-delta)/R0)^2 + 4*epsilon((sigma/(r-delta))^12 - (sigma/(r-delta))^6)) + epsilon",
-        "terms": OrderedDict({ 
-            "K": "energy distance^-2",
-            "r0": "distance",
-            "epsilon": "energy",
-            "sigma": "distance",
-            "delta": "distance"
+        "form": "-0.5*K*R0 ** 2*ln(1-((r-delta)/R0) ** 2 + 4*epsilon((sigma/(r-delta)) ** 12 - (sigma/(r-delta)) ** 6)) + epsilon",
+        "terms": OrderedDict({
+            "K": "[energy] [distance] ** -2",
+            "r0": "[distance]",
+            "epsilon": "[energy]",
+            "sigma": "[distance]",
+            "delta": "[distance]"
         }),
         "description": "This is fene/expand bond"
     },
     "quartic": {
-        "form": "K(r-Rc)^2 * (r-Rc-B1)*(r-Rc-B2) + U0 + 4*epsilon*((sigma/r)^12 - (sigma/r)^6) + epsilon",
+        "form": "K(r-Rc) ** 2 * (r-Rc-B1)*(r-Rc-B2) + U0 + 4*epsilon*((sigma/r) ** 12 - (sigma/r) ** 6) + epsilon",
         "variables": ["r"],
-        "terms": OrderedDict({ 
-            "K": "energy distance^-4",
-            "B1": "distance",
-            "B2": "distance",
-            "Rc": "distance",
-            "U0": "energy"
+        "terms": OrderedDict({
+            "K": "[energy] [distance] ** -4",
+            "B1": "[distance]",
+            "B2": "[distance]",
+            "Rc": "[distance]",
+            "U0": "[energy]"
         }),
         "description": "This is a quartic bond"
     },
     "hybrid": {},  # Special case - allows for more than one bond type in a simulation
     "harmonic": {
-        "form": "K*(r-r0)^2",
+        "form": "K*(r-r0) ** 2",
         "variables": ["r"],
-        "terms": OrderedDict({ 
-            "K": "energy distance^-2",
-            "r0": "distance"
+        "terms": OrderedDict({
+            "K": "[energy] [distance] ** -2",
+            "r0": "[distance]"
         }),
         "description": "This is a harmonic bond"
     },
     "table": {},  # Special case - creation of interpolation tables.
     "class2": {
-        "form": "K2 * (r-r0)^2 + K3 * (r-r0)^3 + K4 * (r-r0)^4",
+        "form": "K2 * (r-r0) ** 2 + K3 * (r-r0) ** 3 + K4 * (r-r0) ** 4",
         "variables": ["r"],
-        "terms": OrderedDict({ 
-            "R0": "distance",
-            "K2": "energy distance^-2",
-            "K3": "energy distance^-3",
-            "K4": "energy distnace^-4"
+        "terms": OrderedDict({
+            "R0": "[distance]",
+            "K2": "[energy] [distance] ** -2",
+            "K3": "[energy] [distance] ** -3",
+            "K4": "[energy] [distance] ** -4"
         }),
         "description": "This is a class2 bond"
     },
     "morse": {
-        "form": "D * (1 - e^(-alpha * (r-r0)))^2",
+        "form": "D * (1 - e ** (-alpha * (r-r0))) ** 2",
         "variables": ["r"],
-        "terms": OrderedDict({ 
-            "D": "energy",
-            "alpha": "distance^-1",
-            "r0": "distance"
+        "terms": OrderedDict({
+            "D": "[energy]",
+            "alpha": "[distance] ** -1",
+            "r0": "[distance]"
         }),
         "description": "This is a class2 bond"
-        
+
     },
 }
 
@@ -103,42 +105,42 @@ _angle_styles = {
     "class2": {
         "terms": ["theta0", "K2", "K3", "K4"],
         "theta0": "degree",
-        "K2": "energy radian^-2",
-        "K3": "energy radian^-3",
-        "K4": "energy radian^-4",
+        "K2": "[energy] radian ** -2",
+        "K3": "[energy] radian ** -3",
+        "K4": "[energy] radian ** -4",
     },
     "cosine/squared": {
         "terms": ["K", "theta0"],
-        "K": "energy",
+        "K": "[energy]",
         "thetea0": "degrees",
     },
     "zero": {},
     "cosine": {
         "terms": ["K"],
-        "K": "energy",
+        "K": "[energy]",
     },
     "harmonic": {
         "terms": ["K", "theta0"],
-        "K": "energy radian^-2",
+        "K": "[energy] radian ** -2",
         "theta0": "degree"
     },
     "hybrid": {},  # Special case - allows for more than one angle type in a simulation
     "cosine/delta": {
         "terms": ["K", "theta0"],
-        "K": "energy",
+        "K": "[energy]",
         "theta0": "degree"
     },
     "table": {},
     "charmm": {
         "terms": ["K", "theta0", "K_ub", "r_ub"],
-        "K": "energy radian^2",
+        "K": "[energy] radian ** 2",
         "theta0": "degree",
-        "K_ub": "energy distance^-2",
-        "r_ub": "distance",
+        "K_ub": "[energy] [distance] ** -2",
+        "r_ub": "[distance]",
     },
     "cosine/periodic": {
         "terms": ["C", "B", "n"],
-        "C": "energy",
+        "C": "[energy]",
         "B": "N/A",
         "n": "N/A"
     },
@@ -148,18 +150,18 @@ _dihedral_styles = {
     "none": {},
     "charmmfsw": {
         "terms": ["K", "n", "d", "weight_factor"],
-        "K": "energy",
+        "K": "[energy]",
         "n": "N/A",  # must be type int, no units
         "d": "degrees",  # must be type int. Differs because units must be degrees regardless of units command ?
         "weight_factor": "N/A"
     },
     "multi/harmonic": {
         "terms": ["A1", "A2", "A3", "A4", "A5"],
-        "A1": "energy",
-        "A2": "energy",
-        "A3": "energy",
-        "A4": "energy",
-        "A5": "energy",
+        "A1": "[energy]",
+        "A2": "[energy]",
+        "A3": "[energy]",
+        "A4": "[energy]",
+        "A5": "[energy]",
     },
     "zero": {},
 
--- eex/translators/lammps_units.py
+++ eex/translators/lammps_units.py
@@ -1,129 +1,123 @@
 # Units based LAMMPS unit styles (http://lammps.sandia.gov/doc/units.html)
 
-
 _units_style = {
     "lj": {},
-
     "real": {
-        "mass": "gram  mol^-1",
-        "distance": "Angstrom",
+        "mass": "gram mol ** -1",
+        "distance": "angstrom",
         "time": "femtosecond",
-        "energy": "kcal mol^-1",
-        "velocity": "Angstrom femtosecond^-1",
-        "force": "kcal  mol^-1 Angstrom^-1",
-        "torque": "kcal  mol^-1",
-        "temperature": "Kelvin",
+        "energy": "kcal mol ** -1",
+        "velocity": "angstrom femtosecond ** -1",
+        "force": "kcal mol ** -1 angstrom ** -1",
+        "torque": "kcal mol ** -1",
+        "temperature": "kelvin",
         "pressure": "atmosphere",
         "dynamic viscosity": "poise",
         "charge": "e",
-        "dipole": "e  Angstrom",
-        "electric field": "volt  Angstrom^-1",
-        "density": "gram  cm^-dim"
-
+        "dipole": "e angstrom",
+        "electric field": "volt angstrom ** -1",
+        "density": "gram cm ** -dim"
     },
-
     "metal": {
-
-        "mass": "gram mol^-1",
-        "distance": "Angstrom",
+        "mass": "gram mol ** -1",
+        "distance": "angstrom",
         "time": "picosecond",
         "energy": "eV",
-        "velocity": "Angstrom  picosecond^-1",
-        "force": "eV Angstrom^-1",
+        "velocity": "angstrom picosecond ** -1",
+        "force": "eV angstrom ** -1",
         "torque": "eV",
-        "temperature": "Kelvin",
+        "temperature": "kelvin",
         "pressure": "bar",
         "dynamic viscosity": "poise",
         "charge": "e",
-        "dipole": "e * Angstrom",
-        "electric field": "volt  Angstrom^-1",
-        "density": "gram  cm^-dim",
+        "dipole": "e * angstrom",
+        "electric field": "volt angstrom ** -1",
+        "density": "gram cm ** -dim",
     },
-
     "si": {
-
         "mass": "kilogram",
         "distance": "meter",
         "time": "second",
-        "energy": "Joule",
-        "velocity": "meter second^-1",
-        "force": "Newtons",
-        "torque": "Newton  meter",
-        "temperature": "Kelvin",
-        "pressure": "Pascal",
-        "dynamic viscosity": "Pascal  second",
-        "charge": "Coulomb",
-        "dipole": "Coulomb meter",
-        "electric field": "volt meter^-1",
-        "density": "kilogram meter^-dim",
+        "energy": "joule",
+        "velocity": "meter second ** -1",
+        "force": "newtons",
+        "torque": "newton meter",
+        "temperature": "kelvin",
+        "pressure": "pascal",
+        "dynamic viscosity": "pascal second",
+        "charge": "coulomb",
+        "dipole": "coulomb meter",
+        "electric field": "volt meter ** -1",
+        "density": "kilogram meter ** -dim",
     },
-
     "cgs": {
         "mass": "gram",
         "distance": "centimeters",
         "time": "second",
         "energy": "ergs",
-        "velocity": "centimeters second^-1",
+        "velocity": "centimeters second ** -1",
         "force": "dyne",
         "torque": "dyne - centimeters",
-        "temperature": "Kelvin",
-        "pressure": "dyne cm^-2",
+        "temperature": "kelvin",
+        "pressure": "dyne cm ** -2",
         "dynamic viscosity": "poise",
-        "charge": "statcoulomb",
-        "dipole": "statcoulomb centimeter",
-        "electric field": "statvolt cm^-1",
-        "density": "gram cm^-dim",
+        "charge": "statcoulombs",
+        "dipole": "statcoulombs centimeter",
+        "electric field": "statvolt cm ** -1",
+        "density": "gram cm ** -dim",
     },
-
     "electron": {
-
-        "mass": "AMU",
+        "mass": "amu",
         "distance": "Bohr",
         "time": "femtosecond",
-        "energy": "Hartree",
-        "velocity": "Bohr (atomic time unit)^-1",
-        "force": "Hartree Bohr^-1",
-        "temperature": "Kelvin",
-        "pressure": "Pascal",
+        "energy": "hartree",
+        "velocity": "Bohr (atomic time unit) ** -1",
+        "force": "hartree Bohr ** -1",
+        "temperature": "kelvin",
+        "pressure": "pascal",
         "charge": "e",
-        "dipole moment": "Debye",
-        "electric field": "volt cm^-1",
+        "dipole moment": "debye",
+        "electric field": "volt cm ** -1",
     },
-
     "micro": {
-
         "mass": "picogram",
         "distance": "micrometer",
         "time": "microsecond",
-        "energy": "picogram  micrometer^2 microsecond^-2",
-        "velocity": "micrometers microsecond^-1",
-        "force": "picogram micrometer microsecond^-2",
-        "torque": "picogram micrometer^2 microsecond^-2",
-        "temperature": "Kelvin",
-        "pressure": "picogram  micrometer^-1 microsecond^-2",
-        "dynamic viscosity": "picogram micrometer^-1 microsecond^-1",
+        "energy": "picogram micrometer ** 2 microsecond ** -2",
+        "velocity": "micrometers microsecond ** -1",
+        "force": "picogram micrometer microsecond ** -2",
+        "torque": "picogram micrometer ** 2 microsecond ** -2",
+        "temperature": "kelvin",
+        "pressure": "picogram micrometer ** -1 microsecond ** -2",
+        "dynamic viscosity": "picogram micrometer ** -1 microsecond ** -1",
         "charge": "picocoulomb",
         "dipole": "picocoulomb micrometer",
-        "electric field": "volt micrometer^-1",
-        "density": "picogram micrometer^-dim"
-
+        "electric field": "volt micrometer ** -1",
+        "density": "picogram micrometer ** -dim"
     },
-
     "nano": {
         "mass": "attogram",
         "distance": "nanometer",
         "time": "nanosecond",
-        "energy": "attogram nanometer^2 nanosecond^-2",
-        "velocity": "nanometers nanosecond^-1",
-        "force": "attogram nanometer nanosecond^-2",
-        "torque": "attogram nanometer^2 nanosecond^-2",
-        "temperature": "Kelvin",
-        "pressure": "attogram nanometer^-1 nanosecond^-2",
-        "dynamic viscosity": "attogram nanometer^-1 nanosecond^-1",
+        "energy": "attogram nanometer ** 2 nanosecond ** -2",
+        "velocity": "nanometers nanosecond ** -1",
+        "force": "attogram nanometer nanosecond ** -2",
+        "torque": "attogram nanometer ** 2 nanosecond ** -2",
+        "temperature": "kelvin",
+        "pressure": "attogram nanometer ** -1 nanosecond ** -2",
+        "dynamic viscosity": "attogram nanometer ** -1 nanosecond ** -1",
         "charge": "e",
         "dipole": "e nanometer",
-        "electric field": "volt nanometer^-1",
-
+        "electric field": "volt nanometer ** -1",
     },
-
 }
+
+if __name__ == "__main__":
+    from eex.units import ureg
+    for k, v in _units_style.items():
+        for k, v in _units_style[k].items():
+            try:
+                u = ureg.parse_expression(v)
+                # print(u)
+            except:
+                print("Pint could not parse %s" % v)
