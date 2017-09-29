951f422aba242aab4a1dfd113e9a49bf90a54c55
Daniel Smith <malorian@me.com>
2017-09-29 15:38:17 -0400

    Fixed find_files to take arbitrary args
    Former-commit-id: 5134be2c44c8bcdd374de89be55bf0d879b0f0d6

--- tests/eex_find_files.py
+++ tests/eex_find_files.py
@@ -8,14 +8,15 @@ test_dir = os.path.dirname(os.path.abspath(__file__))
 examples_dir = os.path.join(test_dir, "..", "examples")
 
 
-def get_example_filename(program, filename):
+def get_example_filename(*filename):
 
+    program = filename[0].lower()
     # Make sure we have written these programs
     if program not in ["amber", "lammps"]:
         raise KeyError("Examples for program %s not found!" % program)
 
     # Make sure file exists
-    fname = os.path.join(examples_dir, program, filename)
+    fname = os.path.join(examples_dir, *filename)
     if not os.path.exists(fname):
         raise OSError("File %s not found!" % fname)
 
