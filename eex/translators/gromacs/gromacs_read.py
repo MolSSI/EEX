f410d04a77c616491fc3f671a1dfa0f85aab2ebc
Daniel Smith <malorian@me.com>
2017-10-18 11:24:14 -0400

    GROMACS: First read and test
    Former-commit-id: 85fe0d6083f75006f1b9791b77aa8f2ea1cf0c30

--- eex/translators/gromacs/gromacs_read.py
+++ eex/translators/gromacs/gromacs_read.py
@@ -3,7 +3,7 @@ GROMACS EEX I/O
 """
 
 import pandas as pd
-import math
+import os
 
 import eex
 
@@ -11,29 +11,39 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def read_gromacs_gro_file(dl, filename):
+def read_gromacs_gro_file(dl, gro_folder):
 
-    box_size = {}
+    ### Read in conf.gro file first
 
-    with open(filename, 'r') as gro_file:
-        title     = next(gro_file)
-        read_size = int(next(gro_file))
-        
+    conf_fname = os.path.join(gro_folder, "conf.gro")
+    if not os.path.exists(conf_fname):
+        raise OSError("GROMACS read: Could not find conf.gro file, expected at '%s'." % conf_fname)
+
+    with open(conf_fname, 'r') as conf_file:
+        conf_title = next(conf_file)
+        conf_read_size = int(next(conf_file))
+
     reader = pd.read_table(
-            filename,
+            conf_fname,
             header=None,
             iterator=True,
-            names=range(6),
+            names=["atom_name", "atomic_symbol", "atom_index", "x", "y", "z"],
             engine="c",
-            comment="#",
+            comment=";",
             delim_whitespace=True,
             skiprows=2,
-            chunksize=read_size
         )
-    data = reader.get_chunk(read_size).dropna(axis=1, how="all")
 
+    data = reader.get_chunk(conf_read_size).dropna(axis=1, how="all")
+    data["atomic_number"] = 0
+    data.set_index("atom_index", inplace=True, drop=True)
+    for val, idx in data.groupby("atomic_symbol"):
+        data.loc[idx.index, "atomic_number"] = eex.metadata.atom_symbol_to_number[val]
+
+    dl.add_atoms(data)
+
+    # Get the box size
     size = reader.get_chunk(1).dropna(axis=1, how="all")
     half_box_length = size.values[0] / 2
-    #Can use .to_dict() instead?
-    box_size = dict(zip(['x', 'y', 'z'], zip(-half_box_length, half_box_length)))
+    box_size = {k: (-v, v) for k, v in zip(["x", "y", "z"], half_box_length)}
     dl.set_box_size(box_size)
--- tests/test_gromacs.py
+++ tests/test_gromacs.py
@@ -1,14 +1,23 @@
 """
-Tests for LAMMPS IO
+Tests for GROMACS IO
 """
 import eex
 import numpy as np
-import os
 import pytest
 import pandas as pd
 import eex_find_files
 
-def test_gromacs_read_gro():
+@pytest.fixture(scope="module")
+def propane_dl():
     dl = eex.datalayer.DataLayer("test_gromacs_read")
-    fname = eex_find_files.get_example_filename("gromacs", "alkanes", "nbutane", "conf.gro")
-    eex.translators.gromacs.read_gromacs_gro_file(dl, fname)
+    gro_folder = eex_find_files.get_example_filename("gromacs", "alkanes", "propane")
+    eex.translators.gromacs.read_gromacs_gro_file(dl, gro_folder)
+    return dl
+
+
+def test_gromacs_read_data(propane_dl):
+    dl = propane_dl
+
+    box_size = dl.get_box_size()
+    assert box_size["x"][0] == pytest.approx(-2.60288, 1.e-6)
+    assert box_size["x"][1] == pytest.approx(2.60288, 1.e-6)
