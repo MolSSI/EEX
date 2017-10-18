d0ca5491266491205c698126d8d6b4b3b04d0acd
Daniel Smith <malorian@me.com>
2017-10-18 11:58:13 -0400

    GROMACS: Builds out overall reader structure
    Former-commit-id: de4dae9a1c8c41ff6ef9733f582cc2035ea46a43

--- eex/translators/gromacs/gromacs_read.py
+++ eex/translators/gromacs/gromacs_read.py
@@ -5,13 +5,20 @@ GROMACS EEX I/O
 import pandas as pd
 import os
 
+import numpy as np
 import eex
 
 import logging
 logger = logging.getLogger(__name__)
 
 
-def read_gromacs_gro_file(dl, gro_folder):
+def read_gromacs_gro_file(dl, gro_folder, ffdir=None):
+
+    if ffdir is None:
+        if "GROMACS_DIR" in os.environ:
+            ffdir = os.environ["GROMACS_DIR"]
+        else:
+            raise KeyError("GROMACS read: Must provide `ffdir` if 'GROMACS_DIR' not in environmental variables.")
 
     ### Read in conf.gro file first
 
@@ -24,15 +31,14 @@ def read_gromacs_gro_file(dl, gro_folder):
         conf_read_size = int(next(conf_file))
 
     reader = pd.read_table(
-            conf_fname,
-            header=None,
-            iterator=True,
-            names=["atom_name", "atomic_symbol", "atom_index", "x", "y", "z"],
-            engine="c",
-            comment=";",
-            delim_whitespace=True,
-            skiprows=2,
-        )
+        conf_fname,
+        header=None,
+        iterator=True,
+        names=["atom_name", "atomic_symbol", "atom_index", "X", "Y", "Z"],
+        engine="c",
+        comment=";",
+        delim_whitespace=True,
+        skiprows=2)
 
     data = reader.get_chunk(conf_read_size).dropna(axis=1, how="all")
     data["atomic_number"] = 0
@@ -47,3 +53,37 @@ def read_gromacs_gro_file(dl, gro_folder):
     half_box_length = size.values[0] / 2
     box_size = {k: (-v, v) for k, v in zip(["x", "y", "z"], half_box_length)}
     dl.set_box_size(box_size)
+
+    ### Read in topol.top file next
+
+    top_fname = os.path.join(gro_folder, "topol.top")
+    if not os.path.exists(conf_fname):
+        raise OSError("GROMACS read: Could not find topol.top file, expected at '%s'." % conf_fname)
+
+    data = pd.read_table(
+        top_fname,
+        header=None,
+        # iterator=True,
+        names=range(9),
+        engine="c",
+        comment=";",
+        delim_whitespace=True)
+
+    print("")
+    data = data.loc[~data.iloc[:, 0].str.contains("#")]
+    indices = np.where(data.iloc[:, 0].str.contains("\["))[0]
+    for indx in range(indices.shape[0] - 1):
+        start = indices[indx]
+        end = indices[indx + 1]
+
+        label = data.iloc[start, 1]
+        tmp = data.iloc[(start + 1):end].convert_objects(convert_numeric=True).dropna(axis=1, how="all")
+        print(label)
+        print(tmp)
+        print('----')
+
+    # print(indices)
+    # print("")
+    # print(data)
+
+    print(os.environ)
--- tests/test_gromacs.py
+++ tests/test_gromacs.py
@@ -7,17 +7,26 @@ import pytest
 import pandas as pd
 import eex_find_files
 
+
 @pytest.fixture(scope="module")
-def propane_dl():
+def nbutane_dl():
     dl = eex.datalayer.DataLayer("test_gromacs_read")
-    gro_folder = eex_find_files.get_example_filename("gromacs", "alkanes", "propane")
+    gro_folder = eex_find_files.get_example_filename("gromacs", "alkanes", "nbutane")
     eex.translators.gromacs.read_gromacs_gro_file(dl, gro_folder)
     return dl
 
 
-def test_gromacs_read_data(propane_dl):
-    dl = propane_dl
+def test_gromacs_read_conf(nbutane_dl):
+    dl = nbutane_dl
 
     box_size = dl.get_box_size()
-    assert box_size["x"][0] == pytest.approx(-2.60288, 1.e-6)
-    assert box_size["x"][1] == pytest.approx(2.60288, 1.e-6)
+    assert box_size["x"][0] == pytest.approx(-2.5737, 1.e-6)
+    assert box_size["x"][1] == pytest.approx(2.5737, 1.e-6)
+
+    data = dl.get_atoms(None)
+    assert data.shape[0] == 4
+    assert dl.get_atom_count() == 4
+
+    assert np.allclose(data["atomic_number"], [6, 6, 6, 6])
+    assert np.allclose(data[["X", "Y", "Z"]].min(axis=0), [-0.147, -0.046, -0.153])
+    assert np.allclose(data[["X", "Y", "Z"]].max(axis=0), [0.0, 0.16, 0.0])
