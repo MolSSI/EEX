3c0817a99d94b7f2b5d56aff3278f7ba7e4c8f97
Daniel Smith <malorian@me.com>
2017-10-20 12:06:59 -0400

    EE: lattice sum docs
    Former-commit-id: d9114f25137de44ff3c4e6c2d9730da9953d191e

--- eex/energy_eval/electrostatics.py
+++ eex/energy_eval/electrostatics.py
@@ -11,6 +11,7 @@ def _coulomb_energy(qij, R):
 
     return np.sum(qij / R)
 
+
 def _fast_coulomb_sum(coords, charges, shift, efunc):
 
     energy = 0.0
@@ -23,7 +24,27 @@ def _fast_coulomb_sum(coords, charges, shift, efunc):
     return 0.5 * energy
 
 
-def lattice_sum(coords, charges, boxlength, nboxes, func="coulomb", return_shells=False, truncation="spherical"):
+def lattice_sum(coords, charges, boxlength, nboxes, func="coulomb", return_shells=False, spherical_truncation=True):
+    """
+    Computes the direct latice sum electostatic energy.
+
+    Parameters
+    ----------
+    coords : array_like
+        A (N, 3) dimensional array of cartesian coordinates
+    charges : array_like
+        A (N,) dimensional array of charges with the order matching the `coords` array.
+    boxlength : array_like
+        A (3,) dimensional array of box sizes (x, y, z)
+    nboxes : int
+        The number of boxes in each direction to sum over
+    spherical_truncation : bool, optional
+        Truncate in roughly a spherical pattern from the home box, otherwise use a 3D box of nboxes in length.
+    func : str, optional
+        The type of operator to sum over
+    return_shells : bool, optional
+        Return the energy of each shell, if false just returns the total energy.
+    """
 
     # Setup boxes
     halfbox = nboxes // 2
@@ -31,9 +52,9 @@ def lattice_sum(coords, charges, boxlength, nboxes, func="coulomb", return_shell
 
     # Setup empty energy dict
     energy = {"home": 0.0}
-    energy.update({k: 0.0 for k in range(1, int(halfbox ** 1.5) + 2)})
+    energy.update({k: 0.0 for k in range(1, int(halfbox**1.5) + 2)})
 
-    # Handle 'home' box
+    # Handle 'home' box, only use lower triangular of pairs
     for i in range(1, coords.shape[0]):
         tmp = coords[:i] - coords[i]
         dR = np.sqrt(np.einsum('ij,ij->i', tmp, tmp))
@@ -42,7 +63,11 @@ def lattice_sum(coords, charges, boxlength, nboxes, func="coulomb", return_shell
     # Image boxes
     for nvecs in itertools.product(boxlist, boxlist, boxlist):
         shell = int(np.linalg.norm(nvecs))
-        if (truncation == "spherical" and (shell > nboxes)) or (shell == 0):
+        # Skip the home box
+        if shell == 0: continue
+
+        # We only want the sphere, not the box
+        if spherical_truncation and (shell > nboxes):
             continue
 
         shiftvec = np.array(nvecs) * boxlength
