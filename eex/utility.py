5a4d9b2e2263f737f70d160549deaac2ce6e8dfa
Daniel Smith <malorian@me.com>
2017-10-20 14:26:28 -0400

    Util: Adds approx hashing function
    Former-commit-id: 99332e5e261f6ea53d5ac5f0b0889e21ad761ee4

--- eex/utility.py
+++ eex/utility.py
@@ -3,22 +3,25 @@ A EEX utility folder
 """
 
 import os
+import hashlib
+
+import numpy as np
 
 
-def line_fuzzy_list(line, ldata):
+def fuzzy_list_match(line, ldata):
     """
     Searches for a line in a list of lines and returns the match if found.
 
     Examples
     --------
 
-    >>> tmp = line_fuzzy_list("thing", ["other", "something words", "else"])
+    >>> tmp = fuzzy_list_match("data tmp", ["other", "data", "else"])
     >>> print(tmp)
-    (True, "something words")
+    (True, "data")
 
-    >>> tmp = line_fuzzy_list("thing", ["other", "else"])
+    >>> tmp = fuzzy_list_match("thing", ["other", "else"])
     >>> print(tmp)
-    (False None)
+    (False, None)
 
     """
 
@@ -53,6 +56,7 @@ def read_lines(filename, nlines, start=0):
 
     return ret_data
 
+
 def find_lowest_hole(data):
     """
     Finds the next lowest value in a list
@@ -65,3 +69,27 @@ def find_lowest_hole(data):
     possible_values = set(range(len(data) + 1))
     new_key = min(possible_values - set(data))
     return new_key
+
+
+def hash(data, ftol=8):
+    """
+    A special hashing function to deal with floating point numbers.
+    """
+
+    # Build up formatters
+    float_fmt = "%." + str(ftol) + "f"
+
+    m = hashlib.md5()
+    for d in data:
+        if isinstance(d, (str)):
+            m.update(d.encode())
+        elif isinstance(d, int):
+            m.update(("%d" % d).encode())
+        elif isinstance(d, (float, np.float64, np.float32)):
+            m.update(("%d" % d).encode())
+        elif isinstance(d, (tuple, list)):
+            m.update(hash(d).encode())
+        else:
+            raise TypeError("hash: Data type '%s' not understood." % type(d))
+
+    return m.hexdigest()
