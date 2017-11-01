a0922eb2e7064c0e71bd45882fe77960211683f9
Jessica Nash <janash@vt.edu>
2017-11-01 11:51:30 -0400

    Clean up amber alkanes directory
    Former-commit-id: 021450234035c6851350c727dd48cc67f9a3606b

--- examples/amber/alkanes/run.sh
+++ examples/amber/alkanes/run.sh
@@ -1,43 +1,40 @@
-cpptraj trappe_ethane_single_molecule.prmtop<<!
-trajin trappe_ethane_single_molecule.inpcrd
-trajout trappe_ethane_single_molecule.nc
-go
-!
+#!/bin/bash 
 
-cpptraj trappe_propane_single_molecule.prmtop<<!
-trajin trappe_propane_single_molecule.inpcrd
-trajout trappe_propane_single_molecule.nc
-go
-!
+# System names
 
-cpptraj trappe_butane_single_molecule.prmtop<<!
-trajin trappe_butane_single_molecule.inpcrd
-trajout trappe_butane_single_molecule.nc
-go
-!
-
-sander -O -i single_point.in -c trappe_ethane_single_molecule.inpcrd -p trappe_ethane_single_molecule.prmtop -ref trappe_ethane_single_molecule.inpcrd -o single_point_ethane.out -r out.rst7 -y trappe_ethane_single_molecule.nc
-
-sander -O -i single_point.in -c trappe_propane_single_molecule.inpcrd -p trappe_propane_single_molecule.prmtop -ref trappe_propane_single_molecule.inpcrd -o single_point_propane.out -r out.rst7 -y trappe_propane_single_molecule.nc
-
-sander -O -i single_point.in -c trappe_butane_single_molecule.inpcrd -p trappe_butane_single_molecule.prmtop -ref trappe_butane_single_molecule.inpcrd -o single_point_butane.out -r out.rst7 -y trappe_butane_single_molecule.nc
-
-#sander -O -i min.in -c trappe_ethane_single_molecule.inpcrd -p trappe_ethane_single_molecule.prmtop -ref trappe_ethane_single_molecule.inpcrd -o single_point_ethane.out -r trappe_ethane_single_molecule.rst7
-
-#sander -O -i min.in -c trappe_propane_single_molecule.inpcrd -p trappe_propane_single_molecule.prmtop -ref trappe_propane_single_molecule.inpcrd -o single_point_propane.out -r trappe_propane_single_molecule.rst7
-
-#sander -O -i min.in -c trappe_butane_single_molecule.inpcrd -p trappe_butane_single_molecule.prmtop -ref trappe_butane_single_molecule.inpcrd -o single_point_butane.out -r trappe_butane_single_molecule.rst7
-
-# Extract energies and clean
 declare -a arr=("ethane" "propane" "butane")
 
+# Start energy calculation file
 echo "Single point energy calculations" > energies.txt
 
 for i in "${arr[@]}"
 do
+	# Run cpptraj to get mol2 of system (assumes one doesn't exist)
+	cpptraj trappe_$(echo ${i})_single_molecule.prmtop<<!
+	trajin trappe_$(echo ${i})_single_molecule.inpcrd
+	trajout $i.mol2
+	go
+!
+
+	tleap <<!
+	loadabmerparams frcmod_trappe.$(echo ${i})
+	sys = loadmols $(echo ${i}).mol2
+	saveamberparm sys trappe_$(echo ${i})_single_molecule.prmtop trappe_$(echo ${i})_single_molecule.inpcrd
+	quit
+!
+	
+	cpptraj trappe_$(echo ${i})_single_molecule.prmtop<<!
+	trajin trappe_$(echo ${i})_single_molecule.inpcrd
+	trajout trappe_$(echo ${i})_single_molecule.nc
+	go
+!
+
+	
+	sander -O -i single_point.in -c trappe_$(echo ${i})_single_molecule.inpcrd -p trappe_$(echo ${i})_single_molecule.prmtop -ref trappe_$(echo ${i})_single_molecule.inpcrd -o single_point_$(echo ${i}).out -r out.rst7 -y trappe_$(echo ${i})_single_molecule.nc
+	
     echo "$i" >> energies.txt
     awk '/NSTEP/, /minimization/' single_point_$i.out >> energies.txt
     echo "" >> energies.txt
 done
 
-rm *.nc
+#rm *.nc
