8e1b09980a7a6d792be55e36e78f2eae3894de51
Jessica Nash <janash@vt.edu>
2017-11-01 16:11:49 -0400

    Update amber cyclic alkanes
    Former-commit-id: 88b26da1fb33686e0ac83e16a12d8bfdf17417c5

--- examples/amber/cyclic/run.sh
+++ examples/amber/cyclic/run.sh
@@ -1,30 +1,37 @@
-cpptraj trappe_cyclopentane_single_molecule.prmtop<<!
-trajin trappe_cyclopentane_single_molecule.inpcrd
-trajout trappe_cyclopentane_single_molecule.nc
-go
-!
+#!/bin/bash 
 
-cpptraj trappe_cyclohexane_single_molecule.prmtop<<!
-trajin trappe_cyclohexane_single_molecule.inpcrd
-trajout trappe_cyclohexane_single_molecule.nc
-go
-!
+# System names
 
-sander -O -i single_point.in -c trappe_cyclopentane_single_molecule.inpcrd -p trappe_cyclopentane_single_molecule.prmtop -ref trappe_cyclopentane_single_molecule.inpcrd -o single_point_cyclopentane.out -r out.rst7 -y trappe_cyclopentane_single_molecule.nc
+declare -a arr=("cyclopentane" "cyclohexane")
 
-sander -O -i single_point.in -c trappe_cyclohexane_single_molecule.inpcrd -p trappe_cyclohexane_single_molecule.prmtop -ref trappe_cyclohexane_single_molecule.inpcrd -o single_point_cyclohexane.out -r out.rst7 -y trappe_cyclohexane_single_molecule.nc
-
-
-# Extract energies and clean
-declare -a arr=("cyclopentane" "cyclohexane" )
-
+# Start energy calculation file
 echo "Single point energy calculations" > energies.txt
 
 for i in "${arr[@]}"
 do
+
+	cat > leap.in <<EOL
+	loadamberparams frcmod_trappe.$(echo ${i})
+	sys = loadmol2 $(echo ${i}).mol2
+	saveamberparm sys trappe_$(echo ${i})_single_molecule.prmtop trappe_$(echo ${i})_single_molecule.inpcrd
+	quit	
+EOL
+	
+	tleap -f leap.in
+	
+	cpptraj trappe_$(echo ${i})_single_molecule.prmtop<<!
+	trajin trappe_$(echo ${i})_single_molecule.inpcrd
+	trajout trappe_$(echo ${i})_single_molecule.nc
+	go
+!
+
+	
+	sander -O -i single_point.in -c trappe_$(echo ${i})_single_molecule.inpcrd -p trappe_$(echo ${i})_single_molecule.prmtop -ref trappe_$(echo ${i})_single_molecule.inpcrd -o single_point_$(echo ${i}).out -r out.rst7 -y trappe_$(echo ${i})_single_molecule.nc
+	
     echo "$i" >> energies.txt
     awk '/NSTEP/, /minimization/' single_point_$i.out >> energies.txt
     echo "" >> energies.txt
 done
 
 rm *.nc
+
