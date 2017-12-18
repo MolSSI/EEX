167f96e741fd6a615357a94528ad1685fa5217f7
janash <janash@vt.edu>
2017-12-18 17:43:22 -0500

    util: start LJ mixing rules
    Former-commit-id: d91aaddb493b52425d72d26f9e313176404e56fe

--- eex/utility.py
+++ eex/utility.py
@@ -91,11 +91,51 @@ def convert_LJ_coeffs(coeffs, origin, final):
     return external
 
 ## LJ Combining Rules
-def _lorentz_berthelot(coeffs, origin):
-    # Convert to sigma epsilon to use combination rules
-    internal = _conversion_matrix[origin][1](coeffs)
-    external = _conversion_matrix["epsilon/sigma"][2](internal)
-    return False
+def _mix_LJ(coeff_i, coeff_j, origin, final):
+    # Calculate interactions between two atom types based on Lorentz-Berthelot mixing rules
+
+    # First convert from input form to internal AB representation
+    internal_coeff_i = convert_LJ_coeffs(coeff_i, origin=origin, final="AB" )
+    internal_coeff_j = convert_LJ_coeffs(coeff_j, origin=origin, final="AB" )
+
+    # Convert from internal AB representation to epsilon/sigma
+    sigma_epsilon_i = convert_LJ_coeffs(internal_coeff_i, origin="AB", final="epsilon/sigma")
+    sigma_epsilon_j = convert_LJ_coeffs(internal_coeff_j, origin="AB", final="epsilon/sigma")
+
+    # Calculate new parameters based on mixing rules
+    ## Call functions here
+
+    # Convert from epsilon-sigma to AB, then to final specified form. Double conversion is necessary because of
+    # form of conversion matrix.
+    convert_params_temp = convert_LJ_coeffs(new_params, origin="epsilon/sigma", final=final)
+    convert_params = _conversion_matrix[final][2](convert_params_temp)
+
+    return convert_params
+
+def _lorentz_berthelot(sigma_epsilon_i, sigma_epsilon_j):
+    new_params = {}
+
+    new_params['sigma'] = (sigma_epsilon_i['sigma'] + sigma_epsilon_j['sigma']) / 2
+    new_params['epsilon'] = (sigma_epsilon_i['epsilon'] * sigma_epsilon_j['sigma']) ** (1. / 2.)
+
+    return new_params
+
+def _geometric(sigma_epsilon_i, sigma_epsilon_j):
+    new_params = {}
+
+    new_params['sigma'] = ( sigma_epsilon_i['sigma'] * sigma_epsilon_j['sigma'] ) ** (1./2.)
+    new_params['epsilon'] = (sigma_epsilon_i['epsilon'] * sigma_epsilon_j['epsilon']) ** (1. / 2.)
+
+    return new_params
+
+def _sixthpower(sigma_epsilon_i, sigma_epsilon_j):
+    new_params = {}
+
+    new_params['sigma'] = ((sigma_epsilon_i['sigma'] ** 6 + sigma_epsilon_j['sigma'] ** 6) / 2 ) ** (1./6.)
+    new_params['epsilon'] = ( 2 * (sigma_epsilon_i['epsilon'] * sigma_epsilon_j['epsilon']) ** (1./2.) *
+                              sigma_epsilon_i['sigma'] ** 3 * sigma_epsilon_j['sigma'] ** 3 )
+    return new_params
+
 
 def fuzzy_list_match(line, ldata):
     """
