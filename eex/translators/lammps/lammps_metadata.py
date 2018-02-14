2de9b50b794cb8d76ae8e4a5d110c3f2d05df687
Eliseo Marin <meliseo@vt.edu>
2018-02-14 15:50:02 -0500

    LAMMPS: Update atom_style metadata from atom-id to atom-index
    Former-commit-id: d943a993f6ef3dc97aa3f114a59e98829b759071

--- eex/translators/lammps/lammps_metadata.py
+++ eex/translators/lammps/lammps_metadata.py
@@ -225,32 +225,32 @@ units_style = {
 # Possible atom styles
 atom_style = {
     "default": "full",
-    "angle": ["atom_ID", "molecule_ID", "atom_type", "x", "y", "z"],
-    "atomic": ["atom_ID", "atom_type", "x", "y", "z"],
-    "body": ["atom_ID", "atom_type", "bodyflag", "mass", "x", "y", "z"],
-    "bond": ["atom_ID", "molecule_ID", "atom_type", "x", "y", "z"],
-    "charge": ["atom_id", "atom_type", "charge", "x", "y", "z"],
-    "dipole": ["atom_id", "atom_type", "charge", "x", "y", "z", "mux", "muy", "muz"],
-    "dpd": ["atom_id", "atom_type", "theta", "x", "y", "z"],
-    "edpd": ["atom_id", "atom_type", "edpd_temp", "edpd_cv", "x", "y", "z"],
-    "mdpd": ["atom_id", "atom_type", "x", "y", "z"],
-    # "tdpd"    ["atom_id", "atom_type", "x", "y", "z", "cc1", "cc2", ccNspecies], #NYI
-    "electron": ["atom_id", "atom_type", "charge", "spin eradius", "x", "y", "z"],
-    "ellipsoid": ["atom_id", "atom_type", "ellipsoidflag density", "x", "y", "z"],
-    "full": ["atom_id", "molecule-ID", "atom_type", "charge", "x", "y", "z"],
-    "line": ["atom_id", "molecule-ID", "atom_type", "lineflag", "density", "x", "y", "z"],
-    "meso": ["atom_id", "atom_type", "rho", "e", "cv", "x", "y", "z"],
-    "molecular": ["atom_id", "molecule-ID"
+    "angle": ["atom_index", "molecule_ID", "atom_type", "x", "y", "z"],
+    "atomic": ["atom_index", "atom_type", "x", "y", "z"],
+    "body": ["atom_index", "atom_type", "bodyflag", "mass", "x", "y", "z"],
+    "bond": ["atom_index", "molecule_ID", "atom_type", "x", "y", "z"],
+    "charge": ["atom_index", "atom_type", "charge", "x", "y", "z"],
+    "dipole": ["atom_index", "atom_type", "charge", "x", "y", "z", "mux", "muy", "muz"],
+    "dpd": ["atom_index", "atom_type", "theta", "x", "y", "z"],
+    "edpd": ["atom_index", "atom_type", "edpd_temp", "edpd_cv", "x", "y", "z"],
+    "mdpd": ["atom_index", "atom_type", "x", "y", "z"],
+    # "tdpd"    ["atom_index", "atom_type", "x", "y", "z", "cc1", "cc2", ccNspecies], #NYI
+    "electron": ["atom_index", "atom_type", "charge", "spin eradius", "x", "y", "z"],
+    "ellipsoid": ["atom_index", "atom_type", "ellipsoidflag density", "x", "y", "z"],
+    "full": ["atom_index", "molecule-ID", "atom_type", "charge", "x", "y", "z"],
+    "line": ["atom_index", "molecule-ID", "atom_type", "lineflag", "density", "x", "y", "z"],
+    "meso": ["atom_index", "atom_type", "rho", "e", "cv", "x", "y", "z"],
+    "molecular": ["atom_index", "molecule-ID"
                   "atom_type", "x", "y", "z"],
-    "peri": ["atom_id", "atom_type", "volume density", "x", "y", "z"],
-    "smd": ["atom_id", "atom_type", "molecule volume", "mass", "kernel_radius", "contact_radius"
+    "peri": ["atom_index", "atom_type", "volume density", "x", "y", "z"],
+    "smd": ["atom_index", "atom_type", "molecule volume", "mass", "kernel_radius", "contact_radius"
             "x", "y", "z"],
-    "sphere": ["atom_id", "atom_type", "diameter", "density", "x", "y", "z"],
-    "template": ["atom_id", "molecule_ID"
+    "sphere": ["atom_index", "atom_type", "diameter", "density", "x", "y", "z"],
+    "template": ["atom_index", "molecule_ID"
                  "template_index", "template_atom", "atom_type", "x", "y", "z"],
-    "tri": ["atom_id", "molecule_ID", "atom_type", "triangleflag", "density", "x", "y", "z"],
-    "wavepacket": ["atom_id", "atom_type", "charge", "spin", "eradius", "etag", "cs_re", "cs_im", "x", "y", "z"],
-    # "hybrid":["atom_id", "atom_type", "x", "y", "z", "sub-style1 sub-style2 ..."]
+    "tri": ["atom_index", "molecule_ID", "atom_type", "triangleflag", "density", "x", "y", "z"],
+    "wavepacket": ["atom_index", "atom_type", "charge", "spin", "eradius", "etag", "cs_re", "cs_im", "x", "y", "z"],
+    # "hybrid":["atom_index", "atom_type", "x", "y", "z", "sub-style1 sub-style2 ..."]
 }
 
 # Units for data labels
