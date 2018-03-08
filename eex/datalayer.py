7fd3cf898e8380aaf6534ba775d8f83523a00428
janash <janash@vt.edu>
2018-03-08 12:35:36 -0500

    DL: additional check in set_pair_scalings
    Former-commit-id: 9ae5f8c00eefa642c4dc32a81c94c63aa78e9df3

--- eex/datalayer.py
+++ eex/datalayer.py
@@ -260,12 +260,15 @@ class DataLayer(object):
         for col in metadata.additional_metadata.nb_scaling["index"]:
             if col not in scaling_df.columns:
                 raise KeyError("%s not found in scaling dataframe (set_pair_scalings)" %(col))
+            
+            if scaling_df[col].dtype != np.int64:
+                raise TypeError("%s column is type %s. Should be type int" %(col, scaling_df[col].dtype) )
         
         # Make sure at least one scaling factor is set
         if len(scaling_df.columns) < 3:
             raise ValueError("No scaling factors set in set_pair_scalings")
-
-        # Check that atom_type are type int
+            if scaling_df[col].dytpe is not np.int64:
+                raise TypeError("Incorrect type")
         
         # Check that scalings are type float
         
