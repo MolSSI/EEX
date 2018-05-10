f28be11dc620a5216e6daf33a5d0ecf90196c18d
Eliseo Marin <meliseo@vt.edu>
2018-05-10 14:44:07 -0400

    Fix bug in wrapper functional form converter

--- eex/form_converters/convert_form.py
+++ eex/form_converters/convert_form.py
@@ -11,22 +11,19 @@ from .convert_helper import sanitize_term_order_name
 
 
 def convert_form(order, coeffs, origin, final):
-
     order = sanitize_term_order_name(order)
 
-    term_md = metadata.get_term_metadata(order, "forms")
+    if order not in (2, 3, 4):
+        raise KeyError("EEX: Term order %s not recognized." % str(order))
 
+    if origin == final:
+        return coeffs
+
+    term_md = metadata.get_term_metadata(order, "forms")
+    # print(coeffs)
     # TODO temporary solution
     to_canonical = '_' + origin + '_to_' + term_md[origin]["canonical_form"]
     from_canonical = '_' + term_md[final]["canonical_form"] + '_to_' + final
-    if order == 2:
-        ret = convert_bond_coeffs(coeffs, origin, final)
-    elif order == 3:
-        ret = convert_angle_coeffs(coeffs, origin, final)
-    elif order == 4:
-        canonical_coeffs = REGISTERED_CONVERTERS[order][to_canonical](coeffs)
-        ret = REGISTERED_CONVERTERS[order][from_canonical](canonical_coeffs)
-    else:
-        raise KeyError("EEX: Term order %s not recognized." % str(order))
-
+    canonical_coeffs = REGISTERED_CONVERTERS[order][to_canonical](coeffs)
+    ret = REGISTERED_CONVERTERS[order][from_canonical](canonical_coeffs)
     return ret
